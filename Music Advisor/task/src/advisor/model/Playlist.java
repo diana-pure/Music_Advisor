0bd9d6729155b964ccad752b0dc96c98be879864
Diana <diana.anisutina@gmail.com>
2020-12-19 21:40:41 +0700

    +featured playlists

--- Music Advisor/task/src/advisor/model/Playlist.java
+++ Music Advisor/task/src/advisor/model/Playlist.java
@@ -9,26 +9,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Playlist implements Representable {
+    private String id;
     private String name;
     private String reference;
 
-    public Playlist(String name, String reference) {
+    public Playlist(String id, String name, String reference) {
+        this.id = id;
         this.name = name;
         this.reference = reference;
     }
 
     public static List<Playlist> fromString(String plain) {
+
         JsonObject jo = JsonParser.parseString(plain).getAsJsonObject();
         JsonArray ja = jo.get("playlists").getAsJsonObject().get("items").getAsJsonArray();
         List<Playlist> playlists = new ArrayList<>(ja.size());
         for (JsonElement e : ja) {
-            playlists.add(new Playlist(e.getAsJsonObject().get("id").getAsString(), e.getAsJsonObject().get("name").getAsString()));
+            String ref = e.getAsJsonObject().get("external_urls").getAsJsonObject().get("spotify").getAsString();
+            playlists.add(new Playlist(
+                    e.getAsJsonObject().get("id").getAsString(),
+                    e.getAsJsonObject().get("name").getAsString(),
+                    ref));
         }
+
         return playlists;
     }
 
     @Override
     public String represent() {
-        return null;
+        return name + "\n" + reference + "\n";
     }
 }
